fb1ec8a67fa1bc3b4881b46a106ddd347219e7fc
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2023-02-27 09:15:32 -0300

    Update server.py

--- server.py
+++ server.py
@@ -3,34 +3,36 @@ from constRPYC import *
 from rpyc.utils.server import ThreadedServer
 
 class DBList(rpyc.Service):
-    def __init__(self):
-        self.value = []
+  value = []
 
-    def exposed_append(self, data):
-        self.value.append(data)
-        return self.value
+  def exposed_append(self, data):
+    self.value = self.value + [data]
+    return self.value
 
-    def exposed_value(self):
-        return self.value
-
-    def exposed_search(self, data):
-        if data in self.value:
-            print(f"O elemento {data} está na lista")
-        else:
-            print(f"O elemento {data} não está na lista")
-
-    def exposed_remove(self, data):
-        self.value.remove(data)
-        return self.value
-
-    def exposed_insert(self, index, data):
+   def exposed_insert(self, index, data):
         self.value.insert(index, data)
         return self.value
+    
+  def exposed_remove(self, data):
+    if data in self.value:
+      self.value.remove(data)
+      return self.value
+    else:
+      return "Elemento não encontrado na lista"
 
-    def exposed_sort(self):
-        self.value.sort()
-        return self.value
+  def exposed_search(self, data):
+    if data in self.value:
+      return "Elemento encontrado na posição " + str(self.value.index(data))
+    else:
+      return "Elemento não encontrado na lista"
+
+  def exposed_sort(self):
+    self.value.sort()
+    return self.value
+
+  def exposed_value(self):
+    return self.value
 
 if __name__ == "__main__":
-    server = ThreadedServer(DBList(), port=PORT)
-    server.start()
+  server = ThreadedServer(DBList(), port = PORT)
+  server.start()
