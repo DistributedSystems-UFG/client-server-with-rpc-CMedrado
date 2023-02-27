e167a0caaca878bf3d114ac25bfa3c240a7cd34b
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2023-02-27 09:34:03 -0300

    Update server.py

--- server.py
+++ server.py
@@ -2,38 +2,40 @@ import rpyc
 from constRPYC import *
 from rpyc.utils.server import ThreadedServer
 
-class DBList(rpyc.Service):
-  def __init__(self):
-        self.value = []
-        
-  def exposed_append(self, data):
-    self.value = self.value + [data]
-    return self.value
 
-  def exposed_insert(self, index, data):
+class DBList(rpyc.Service):
+    def __init__(self):
+        self.value = []
+
+    def exposed_append(self, data):
+        self.value = self.value + [data]
+        return self.value
+
+    def exposed_insert(self, index, data):
         self.value.insert(index, data)
         return self.value
-    
-  def exposed_remove(self, data):
-    if data in self.value:
-      self.value.remove(data)
-      return self.value
-    else:
-      return "Elemento não encontrado na lista"
 
-  def exposed_search(self, data):
-    if data in self.value:
-      return "Elemento encontrado na posição " + str(self.value.index(data))
-    else:
-      return "Elemento não encontrado na lista"
+    def exposed_remove(self, data):
+        if data in self.value:
+            self.value.remove(data)
+            return self.value
+        else:
+            return "Elemento não encontrado na lista"
 
-  def exposed_sort(self):
-    self.value.sort()
-    return self.value
+    def exposed_search(self, data):
+        if data in self.value:
+            return "Elemento encontrado na posição " + str(self.value.index(data))
+        else:
+            return "Elemento não encontrado na lista"
 
-  def exposed_value(self):
-    return self.value
+    def exposed_sort(self):
+        self.value.sort()
+        return self.value
+
+    def exposed_value(self):
+        return self.value
+
 
 if __name__ == "__main__":
-  server = ThreadedServer(DBList(), port = PORT)
-  server.start()
+    server = ThreadedServer(DBList(), port=PORT)
+    server.start()
