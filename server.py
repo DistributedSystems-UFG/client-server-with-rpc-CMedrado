ffc6fa01ea4d5312709ee91fc7506363b7d75376
Rafael Medrado <72144146+CMedrado@users.noreply.github.com>
2023-02-26 22:28:08 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,36 @@
 import rpyc
-from constRPYC import * #-
+from constRPYC import *
 from rpyc.utils.server import ThreadedServer
 
 class DBList(rpyc.Service):
-  value = []
+    value = []
 
-  def exposed_append(self, data):
-    self.value = self.value + [data]
-    return self.value
+    def exposed_append(self, data):
+        self.value = self.value + [data]
+        return self.value
 
-  def exposed_value(self):
-    return self.value
+    def exposed_value(self):
+        return self.value
+
+    def exposed_search(self, data):
+        result = []
+        for item in self.value:
+            if data in item:
+                result.append(item)
+        return result
+
+    def exposed_remove(self, data):
+        self.value = [item for item in self.value if data not in item]
+        return self.value
+
+    def exposed_insert(self, data, index):
+        self.value.insert(index, data)
+        return self.value
+
+    def exposed_sort(self):
+        self.value = sorted(self.value)
+        return self.value
 
 if __name__ == "__main__":
-  server = ThreadedServer(DBList(), port = PORT)
-  server.start()
-
+    server = ThreadedServer(DBList(), port=PORT)
+    server.start()
